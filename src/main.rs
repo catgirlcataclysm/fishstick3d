41c1ba79bd8f9201a4064989481f248c26b13dd2
kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2023-10-15 23:39:40 -0400

    woah testing winit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{EventLoop, ControlFlow},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    window.set_title("boobies");
+
+    event_loop.run(move | event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    }).unwrap();
+}
